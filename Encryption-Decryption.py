94c45eda165162620419902daa9bcec1c8759d2c
Ch. Theodorou <theodoroucharalabos@gmail.com>
2018-01-31 16:22:46 +0200

    Update Encryption-Decryption.py

--- Encryption-Decryption.py
+++ Encryption-Decryption.py
@@ -2,6 +2,14 @@ from Crypto.Cipher import AES
 
 
 
+messageClear= input("test me: ")
+#print the messages length
+print(len(messageClear))
+#we need a message that is multiplier of 16
+while(len(messageClear)%16!=0):
+    messageClear = messageClear +"~"
+    
+
 #secret_key = os.urandom(BLOCK_SIZE) i generated once and i will use that always
 secret_key=b'b\x90\xf7\x1d\\KY\xc3\xd7\x13\xf1\x90\xba\xe4HS\xe3\xce\x1cd\x8f\xdf\xda\xc8u\xa9B\x85-&<\xb7'
 Initialization_vector = 'This is a testIV'#IV must be 16 bytes long
@@ -17,7 +25,8 @@ def decryption(ciphertext):
     return obj2.decrypt(ciphertext).decode()
 
 if __name__=="__main__":
-    message = encryption("The answer is no")#The message must be a  be a multiple of 16 bytes
+    message = encryption(messageClear)#The message must be a  be a multiple of 16 bytes
     print(message)
     message = decryption(message)
-    print("Decrypted successful:" + message)
+    message = message.replace("~","")
+print("Decrypted successful:" + message)
