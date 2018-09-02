90005cfb1fee4f86d94291d2de2c3bfb92e213e2
Ch. Theodorou <theodoroucharalabos@gmail.com>
2018-09-02 11:13:48 +0300

    Update Encryption-Decryption.py

--- Encryption-Decryption.py
+++ Encryption-Decryption.py
@@ -7,12 +7,12 @@ messageClear= input("test me: ")
 print(len(messageClear))
 #we need a message that is multiplier of 16
 while(len(messageClear)%16!=0):
-    messageClear = messageClear +"~"
-    
+    messageClear = messageClear +" "
+messageClear =  messageClear.encode()
 
 #secret_key = os.urandom(BLOCK_SIZE) i generated once and i will use that always
 secret_key=b'b\x90\xf7\x1d\\KY\xc3\xd7\x13\xf1\x90\xba\xe4HS\xe3\xce\x1cd\x8f\xdf\xda\xc8u\xa9B\x85-&<\xb7'
-Initialization_vector = 'This is a testIV'#IV must be 16 bytes long
+Initialization_vector = b'This is a testIV'#IV must be 16 bytes long
 
 def encryption(messageToEncrypt):
     obj = AES.new(secret_key, AES.MODE_CBC,Initialization_vector)
